35bde2f5f36c73fd99939daed23b354c67cbe204
JOYBOY <114289165+Ameya-kawade@users.noreply.github.com>
2023-10-03 19:17:54 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,17 +1,19 @@
 import requests
 
 def get_Weather_Status() :
-    
-    api_key = "6f76b51e06c848808c5113655230110"
+
+    api_key = "your API KEY here"
     
     city = input("Enter city name : \n")
 
     print("\n")
-
+    # Url used for sending get request
     url = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q={city}&aqi=no"
 
+    # Using the get method from the requests module, it creates a get request to get the data for the given city
     response = requests.get(url)
-
+    
+    # It checks if the get request was successfull if yes then the data sent back is converted to json and printed
     if response.status_code == 200 :
 
         data = response.json()
